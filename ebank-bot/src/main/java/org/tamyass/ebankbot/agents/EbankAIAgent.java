fd5cd2648e2801ad27ad81e02c799c7ded2b7849
natisenissay <yassine.tmnt@gmail.com>
2026-03-10 16:59:10 +0100

    add ToolCallbackProvider pour bloquer les questions

--- ebank-bot/src/main/java/org/tamyass/ebankbot/agents/EbankAIAgent.java
+++ ebank-bot/src/main/java/org/tamyass/ebankbot/agents/EbankAIAgent.java
@@ -3,6 +3,7 @@ package org.tamyass.ebankbot.agents;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,9 +11,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class EbankAIAgent {
     private ChatClient chatClient;
 
-    public EbankAIAgent(ChatClient.Builder chatClient, ChatMemory chatMemory) {
+    public EbankAIAgent(ChatClient.Builder chatClient,
+                        ChatMemory chatMemory,
+                        ToolCallbackProvider tools) {
         this.chatClient = chatClient
+                .defaultSystem("""
+                        Vous un assistant qui se charge de répondre aux question
+                        de l'utilisateur à propos des clients et des comptes bancaires, en fonction du contexte fourni.
+                        Si aucun contexte n'est fourni, répond avec JE NE SAIS PAS
+                        """)
                 .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build())
+                .defaultToolCallbacks(tools)
                 .build();
     }
     public String chat(String query){
